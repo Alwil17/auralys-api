4e0afdf01684d324c737d16a726ea67acaf868f9
alwil17 <willialfred24@gmail.com>
2025-06-27 17:55:22 +0000

    style: improve code formatting and consistency in user_dto.py

--- app/schemas/user_dto.py
+++ app/schemas/user_dto.py
@@ -2,6 +2,7 @@ from pydantic import BaseModel, ConfigDict, EmailStr, Field
 from typing import Optional
 from datetime import datetime, date
 
+
 class UserCreateDTO(BaseModel):
     name: str = Field(..., max_length=100)
     email: EmailStr
@@ -10,12 +11,14 @@ class UserCreateDTO(BaseModel):
     image_url: Optional[str] = None
     consent: Optional[int] = 1
 
+
 class UserUpdateDTO(BaseModel):
     name: Optional[str] = Field(None, max_length=100)
     email: Optional[EmailStr] = None
     role: Optional[str] = None
     password: Optional[str] = None
     consent: Optional[int] = 1
+
 
 class UserResponse(BaseModel):
     id: int
@@ -27,4 +30,3 @@ class UserResponse(BaseModel):
     updated_at: datetime
 
     model_config = ConfigDict(from_attributes=True)
-
