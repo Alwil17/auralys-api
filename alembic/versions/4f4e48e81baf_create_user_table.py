0ef7d46f91cfde234c6ea673307b05b8a41f80a2
alwil17 <willialfred24@gmail.com>
2025-06-27 17:19:26 +0000

    feat: create migration script for user table

---
+++ alembic/versions/4f4e48e81baf_create_user_table.py
@@ -0,0 +1,28 @@
+"""create user table
+
+Revision ID: 4f4e48e81baf
+Revises: 
+Create Date: 2025-06-27 17:18:51.456283
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '4f4e48e81baf'
+down_revision: Union[str, Sequence[str], None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    pass
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    pass
