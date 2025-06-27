0f802f0694254ce63728e435f94f0b67a6bc3020
alwil17 <willialfred24@gmail.com>
2025-06-27 17:54:15 +0000

    style: fix formatting inconsistencies in create_user_table.py

--- alembic/versions/4f4e48e81baf_create_user_table.py
+++ alembic/versions/4f4e48e81baf_create_user_table.py
@@ -1,10 +1,11 @@
 """create user table
 
 Revision ID: 4f4e48e81baf
-Revises: 
+Revises:
 Create Date: 2025-06-27 17:18:51.456283
 
 """
+
 from typing import Sequence, Union
 
 from alembic import op
@@ -12,7 +13,7 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '4f4e48e81baf'
+revision: str = "4f4e48e81baf"
 down_revision: Union[str, Sequence[str], None] = None
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
