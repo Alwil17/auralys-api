a4270ae31ab59d4c2ebb27c48f92162f0115773d
alwil17 <willialfred24@gmail.com>
2025-07-03 08:37:55 +0000

    fix: update User model to allow nullable name and clean up comments

--- app/db/models/user.py
+++ app/db/models/user.py
@@ -10,12 +10,14 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100), nullable=False)
+    name = Column(String(100), nullable=True)
     email = Column(String(100), unique=True, index=True, nullable=False)
-    hashed_password = Column(String(128), nullable=False)  # stocke hashed password
+    hashed_password = Column(String(128), nullable=False)
     role = Column(String(50), default="user")
     image_url = Column(String(500), nullable=True)
-    consent = Column(Integer, default=1)  # 1  =autorise collecte, 0 = local
+    consent = Column(Integer, default=1)
+    age = Column(Integer, default=0)
+    gender = Column(String(10), nullable=True)
     created_at = Column(DateTime, default=datetime.now(timezone.utc))
     updated_at = Column(
         DateTime,
